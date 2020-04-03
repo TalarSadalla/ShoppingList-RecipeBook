45bc3bc6b8d62e375ac8cc67137b1c403b103a09
TalarSadalla <talarsadalla@yahoo.com>
2020-04-03 12:10:06 +0200

    Added recipe service
    # Conflicts:
    #	src/app/app.module.ts
    #	src/app/recipes/recipe-detail/recipe-detail.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'; 
+import { from } from 'rxjs';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -32,7 +34,7 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
